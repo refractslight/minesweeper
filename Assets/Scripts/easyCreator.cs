39c5a57138fc877ea11aace102f0dc7e185a09bd
Susie <susie.odneal@gmail.com>
2018-01-20 09:35:08 -0600

    Created "Chessboard"

---
+++ Assets/Scripts/easyCreator.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class tiles
+//{
+//
+//	public GameObject gameObject;
+//	public Material tileMat;
+//
+//
+//}
+
+
+public class easyCreator : MonoBehaviour
+{
+
+	public GameObject tilePrefab;
+	public int columns;
+	public int rows;
+	public float padding = 0.5f;
+//	public Material[] materialList = new Material[2];
+//	int matIndex = 0;
+	public Color tile1 = new Vector4 (171, 120, 73, 255);
+	public Color tile2 = new Vector4 (75, 52, 32, 255);
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int z = 0; z < columns; z = z + 1) {
+			//matIndex = z % 2;
+			for (int x = 0; x < rows; x = x + 1) {
+				GameObject _clone = Instantiate (tilePrefab, new Vector3 (x, 0, z), Quaternion.identity) as GameObject;
+//				Material _mat = _clone.GetComponent<Material> ();
+
+//				_mat.color
+
+				if (x % 2 == 0) {
+					if (z % 2 == 0) {
+						_clone.GetComponent<Renderer> ().material.color = tile1;
+						//Debug.Log("Tile Colors1");
+					} else {
+						_clone.GetComponent<Renderer> ().material.color = tile2;
+						//Debug.Log("Tile Colors2");
+					}
+				} else {
+					if (z % 2 == 0) {
+						_clone.GetComponent<Renderer> ().material.color = tile2;
+						//Debug.Log("Tile Colors3");
+					} else {
+						_clone.GetComponent<Renderer> ().material.color = tile1;
+						//	Debug.Log("Tile Colors4");
+					}
+				}
+
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
+
